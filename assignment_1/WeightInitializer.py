6b35e85b2c324586b2e82a785fec012ecd0618c9
RohitChoudhary02 <86179534+RohitChoudhary02@users.noreply.github.com>
2022-02-22 05:26:03 +0530

    Update WeightInitializer.py

--- assignment_1/WeightInitializer.py
+++ assignment_1/WeightInitializer.py
@@ -3,8 +3,26 @@ import numpy as np
 This is a function repository, all the initializers will be implemented here as functions
 '''
 
-def get_weights_random(input_size, output_size):
-    return np.random.randn(input_size, output_size)
+class Random_weight_bias():
+    def __init__(self):
+        pass
 
-def get_bias_random(output_size):
-    return np.random.randn(1, output_size)
+    def get_weights_random(self,input_size,output_size):
+        return np.random.randn(input_size, output_size)
+
+    def get_bias_random(self,output_size):
+        return np.random.rand(1, output_size)
+
+class Xavier_weight_bias():
+    def __init__(self):
+        pass
+
+    #write for xavier
+
+
+#def get_weights_random(input_size, output_size):
+#    return np.random.randn(input_size, output_size)
+
+
+#def get_bias_random(output_size):
+#    return np.random.randn(1, output_size)
