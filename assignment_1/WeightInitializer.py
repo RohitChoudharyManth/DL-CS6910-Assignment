a22fa2c1e0b6e8cefaccaacdfa6232df6a0c5077
RohitChoudhary02 <86179534+RohitChoudhary02@users.noreply.github.com>
2022-02-22 05:39:33 +0530

    Update WeightInitializer.py

--- assignment_1/WeightInitializer.py
+++ assignment_1/WeightInitializer.py
@@ -17,6 +17,17 @@ class Xavier_weight_bias():
     def __init__(self):
         pass
 
+    def get_weights_xavier(self,input_size,output_size):
+        #normalized xavier weight intialization
+        # calculate the range for the weights
+        lower, upper = -(sqrt(6.0) / sqrt(input_size + output_size)), (sqrt(6.0) / sqrt(input_size + output_size))
+        return np.random.uniform(low=lower, high=upper, size=(input_size, output_size))
+
+    def get_bias_xavier(self,input_size,output_size):
+        lower, upper = -(sqrt(6.0) / sqrt(input_size + output_size)), (sqrt(6.0) / sqrt(input_size + output_size))
+        return np.random.uniform(low=lower, high=upper, size=(1, output_size))
+
+
     #write for xavier
 
 
