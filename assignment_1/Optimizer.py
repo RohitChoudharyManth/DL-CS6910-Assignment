7f28b810f6721c467d19e77e88e5eeb9078d2f91
RohitChoudhary02 <86179534+RohitChoudhary02@users.noreply.github.com>
2022-02-22 09:36:22 +0530

    Update Optimizer.py

--- assignment_1/Optimizer.py
+++ assignment_1/Optimizer.py
@@ -4,11 +4,28 @@ Optimizer class, implement all the optimizers as methods of this class and add i
 
 
 class Optimizer:
-    def __init__(self, learning_rate, optimizer='SGD'):
+    def __init__(self,  optimizer='SGD', learning_rate = 1e-2, gamma = 0.9, epsilon = 1e-8, beta1=0.9, beta2=0.999):
         self.lr = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.update = 0
+        self.m = 0
+        self.v = 0
 
         if optimizer == 'SGD':
             self.optimizer = self.SGD_optim
+        elif optimizer == 'Momentum':
+            self.optimizer = self.momentum_optim
+        elif optimizer == 'nesterov':
+            self.optimizer = self.nesterov_optim
+        elif optimizer == 'rmsprop':
+            self.optimizer = self.rmsprop_optim
+        elif optimizer == 'adam':
+            self.optimizer == self.adam_optim
+        elif optimizer == 'nadam':
+            self.optimizer = self.nadam_optim
         else:
             print('Give correct optimizer name')
             raise Exception
@@ -16,4 +33,11 @@ class Optimizer:
     def SGD_optim(self, W, B, W_error, B_error):
         W -= self.lr*W_error
         B -= self.lr * B_error
+        return W, B
+
+    def momentum_optim(self, W, B, W_error, B_error):
+        self.update = self.gamma * self.update + self.lr * W_error
+        W -= self.update
+        # Similarly
+        B -= self.gamma * self.update + self.lr * B_error
         return W, B
